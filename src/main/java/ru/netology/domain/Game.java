93bd44c65bf4359da5c4a22937f18a899742d5fa
boarderbare <boarderbare@gmail.com>
2022-06-23 15:52:19 +0400

    chenge search on map

--- src/main/java/ru/netology/domain/Game.java
+++ src/main/java/ru/netology/domain/Game.java
@@ -4,31 +4,28 @@ import java.util.*;
 
 public class Game {
 
-    private Map<String,Integer> map = new HashMap<>();
+    private Map<String, Integer> map = new HashMap<>();
 
     public void register(String name, int strenght) {
         map.put(name, strenght);
     }
 
-    public Map<String, Integer> getAll(){
+    public Map<String, Integer> getAll() {
         return map;
     }
 
-    public int findByName(String name) {
-        for (String key : map.keySet()) {
-            if (key.equals(name)){
-                return map.get(key);
-            }
-        }
-        throw new NotRegisteredException("This player " + name + "is not registered");
-    }
-
     public int round(String playerName1, String playerName2) {
+        if (!map.containsKey(playerName1)) {
+            throw new NotRegisteredException("This player " + playerName1 + "is not registered");
+        }
+        if (!map.containsKey(playerName2)) {
+            throw new NotRegisteredException("This player " + playerName2 + "is not registered");
+        }
 
-        if (findByName(playerName1) > findByName(playerName2)) {
+        if (map.get(playerName1) > map.get(playerName2)) {
             return 1;
         }
-        if (findByName(playerName1) < findByName(playerName2)) {
+        if (map.get(playerName1) < map.get(playerName2)) {
             return 2;
         }
         return 0;
